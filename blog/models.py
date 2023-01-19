2240e682c59608a4ee90ae8170eefb855dfcc060
jagi113 <jaroslavgirovsky@gmail.com>
2023-01-19 14:17:51 +0100

    autopopulating-slug

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     image = models.URLField(max_length = 200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, null=False, db_index=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
