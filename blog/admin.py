cd10f6d2f168acfea8dc2536639c61d081bf90d2
jagi113 <jaroslavgirovsky@gmail.com>
2023-01-20 13:54:18 +0100

    multi-relational database

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Tag, Author
 
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",) }
+    list_filter = ("author", )
+    list_display = ("title", "author")
     
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -7,18 +7,38 @@ STATUS = (
     (1,"Publish")
 )
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200, default=User, null=False, unique=True)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.nickname
+    
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.caption
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    image = models.URLField(max_length = 200)
+    image = models.URLField(max_length = 400)
     slug = models.SlugField(max_length=200, unique=True, null=False, db_index=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name='blog_post')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    tags = models.ManyToManyField(Tag)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+    
+
