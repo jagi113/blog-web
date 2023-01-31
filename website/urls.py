533d47fb2ade3833cebf895741645391e8f28a25
jagi113 <jaroslavgirovsky@gmail.com>
2023-01-31 12:59:08 +0100

    finishing-AWSdeployment

--- website/urls.py
+++ website/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url="blog/")),
     path("blog/", include("blog.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
